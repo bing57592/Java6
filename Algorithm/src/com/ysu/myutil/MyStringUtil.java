ac3173c772448e3e97e42c1f55adfd390a864595
陈宪东 <752890864@qq.com>
2018-08-30 09:40:29 +0800

    涨知识

--- Algorithm/src/com/ysu/myutil/MyStringUtil.java
+++ Algorithm/src/com/ysu/myutil/MyStringUtil.java
@@ -5,5 +5,8 @@ import org.junit.Test;
 public class MyStringUtil {
     public static String DEFAULT_LINEAE = "----------";
 
+    public static void timeUsed(Long second) {
+        System.out.println("总共花费了 " + second + " 毫秒");
+    }
 
 }
